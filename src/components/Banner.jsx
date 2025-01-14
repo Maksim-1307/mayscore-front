50f7fd24f2d835152bc9d6b9ea8557dfda54bb28
Maksim-1307 <max.07mal@gmail.com>
2025-01-14 09:09:41 +0300

    banner link changed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,7 +14,7 @@ function Banner ({className}) {
     }
     return (
         <div ref={bannerRef} className={`${className ? className : ''} banner`}>
-            <a href="https://af-bk6bba.com/click?offer_id=692&partner_id=3257&landing_id=2323&utm_medium=Affiliate" >
+            <a href="https://clicks.af-ru2e2e.com/click?offer_id=316&partner_id=3257&landing_id=4378&utm_medium=affiliate&banner_id=4140" >
                 <img src={banner} alt="" />
             </a>
             <button className='banner__close-button' onClick={() => closeSelf()}>
