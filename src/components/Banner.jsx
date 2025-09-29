de337c7a388be6912189b0254ce6eeeaf11f5953
Maksim Maltsev <maksimmaltsev@Maksims-MacBook-Air-2.local>
2025-09-29 21:19:40 +0300

    new banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,4 @@
-import banner from '../images/mobile-banner.jpeg'
+import banner from '../images/new-banner.jpeg'
 import cross from '../images/icons/cross.svg';
 
 import React, { useRef } from 'react';
@@ -14,7 +14,7 @@ function Banner ({className}) {
     }
     return (
         <div ref={bannerRef} className={`${className ? className : ''} banner`}>
-            <a href="https://clicks.af-ru2e2e.com/click?offer_id=316&partner_id=3257&landing_id=4378&utm_medium=affiliate&banner_id=4140" >
+            <a href="https://clicks.af-ru2e2e.com/click?offer_id=316&partner_id=3257&landing_id=3990&utm_medium=affiliate&web_master_id={web_master_id}&partner_click_id={partner_click_id}" >
                 <img src={banner} alt="" />
             </a>
             <button className='banner__close-button' onClick={() => closeSelf()}>
@@ -23,5 +23,7 @@ function Banner ({className}) {
         </div>
     );
 }
+
+// old link https://clicks.af-ru2e2e.com/click?offer_id=316&partner_id=3257&landing_id=4378&utm_medium=affiliate&banner_id=4140
 
 export default Banner;
