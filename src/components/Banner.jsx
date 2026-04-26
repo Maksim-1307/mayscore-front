ccc7ade9effd2fe94f15108cd45d0752dbefa781
Maksim Maltsev <maksimmaltsev@Maksims-MacBook-Air-2.local>
2026-04-26 14:03:56 +0300

    feat: support changing banner from admin panel

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,29 +1,61 @@
 import banner from '../images/new-banner.jpeg'
 import cross from '../images/icons/cross.svg';
 
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+
+const BANNER_URL = 'https://mayscor.ru/admin/get_banners.php';
+const BANNER_BASE_URL = 'https://mayscor.ru/admin/';
 
 function Banner ({className}) {
 
     const bannerRef = useRef(null);
+
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [bannerLink, setBannerLink] = React.useState(null);
+    const [bannerImageUrl, setBannerImageUrl] = React.useState(null);
+
+    useEffect(() => {
+        const fetchBanner = async () => {
+            try {
+                setIsLoading(true);
+                const response = await fetch(BANNER_URL);
+                const textData = await response.text();
+                const jsonData = JSON.parse(textData)[0];
+                setBannerLink(jsonData?.link);
+                setBannerImageUrl(jsonData?.image_url);
+            } catch (error) {
+                console.error('Ошибка загрузки баннера:', error);
+                setBannerImageUrl(null);
+                setBannerLink(null);
+                setTimeout(fetchBanner, 1500);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchBanner();
+    }, []);
 
     function closeSelf(){
         if (bannerRef.current) {
             bannerRef.current.style.display = 'none';
         }
     }
-    return (
-        <div ref={bannerRef} className={`${className ? className : ''} banner`}>
-            <a href="https://refpa27918.com/L?tag=s_5489662m_355c_&site=5489662&ad=355" >
-                <img src={banner} alt="" />
-            </a>
-            <button className='banner__close-button' onClick={() => closeSelf()}>
-                <img src={cross} alt="" />
-            </button>
-        </div>
-    );
-}
 
-// old link https://clicks.af-ru2e2e.com/click?offer_id=316&partner_id=3257&landing_id=4378&utm_medium=affiliate&banner_id=4140
+    if (bannerImageUrl && bannerLink) {
+        return (
+            <div ref={bannerRef} className={`${className ? className : ''} banner`}>
+                <a href={bannerLink} >
+                    <img src={`${BANNER_BASE_URL}/${bannerImageUrl}`} alt="" />
+                </a>
+                <button className='banner__close-button' onClick={() => closeSelf()}>
+                    <img src={cross} alt="" />
+                </button>
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
 
 export default Banner;
